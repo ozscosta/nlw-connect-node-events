2580566879d049df9b60725f7bceb5562546596f
Ozeias Costa <costa.ozeias@gmail.com>
2025-02-18 01:11:18 -0300

    feat: configura biomejs

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -1,10 +1,14 @@
-import { FastifyInstance } from "fastify";
-import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import type { FastifyInstance } from 'fastify'
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 
 export const hello: FastifyPluginAsyncZod = async (app: FastifyInstance) => {
-    app.get('/hello', {
-        schema: {
-            tags: ['hello'],
-        },
-    }, async () => "hello world");
+  app.get(
+    '/hello',
+    {
+      schema: {
+        tags: ['hello'],
+      },
+    },
+    async () => 'hello world'
+  )
 }
--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,41 @@
-import { fastifyCors } from "@fastify/cors";
-import { fastifySwagger } from "@fastify/swagger";
-import { fastifySwaggerUi } from "@fastify/swagger-ui";
-import { fastify } from "fastify";
-import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
-import { env } from "./env";
-import { hello as helloRoute } from "./routes/hello";
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import { hello as helloRoute } from './routes/hello'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-    origin: true,
-});
+  origin: true,
+})
 
 app.register(fastifySwagger, {
-    openapi: {
-        info: {
-            title: 'NLW Connect',
-            version: '0.0.1',
-        },
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
     },
-    transform: jsonSchemaTransform
-});
+  },
+  transform: jsonSchemaTransform,
+})
 
 app.register(fastifySwaggerUi, {
-    routePrefix: '/docs',
-});
+  routePrefix: '/docs',
+})
 
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
-app.register(helloRoute);
+app.register(helloRoute)
 
 app.listen({ port: env.PORT }).then(() => {
-    console.log(`Server is running on port ${env.PORT}`);
-});
+  console.log(`Server is running on port ${env.PORT}`)
+})
